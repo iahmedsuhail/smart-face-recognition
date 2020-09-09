97c1a547f8882aec0f2f01a3420b4064476e4b9c
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-09 15:11:16 +1200

    Add state to InputLinkForm
    - InputLinkForm can now have a state
    - Future Work: Add functionality to make API Calls to Clarifai

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
