28d6ae3da86a312988adbbcb4d9369ce3d0f9dba
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-09 15:54:56 +1200

    Update Clarifai to use Face Detection Model
    - The response is now a bounding box, that encovers the face
    - TBD: Use the bounding box to wrap the face in the image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation/Navigation.js";
 import Logo from "./components/Logo/Logo.js";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "./App.css";
 import Clarifai from 'clarifai';
 
@@ -16,20 +17,21 @@ class App extends React.Component {
 
     this.state = {
       input: "",
+      imageUrl: '',
     };
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   };
 
   onButtonSubmit = () => {
-    console.log("click");
+    this.setState({imageUrl: this.state.input})
 
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", ["https://samples.clarifai.com/puppy.jpeg"])
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, [this.state.input])
     .then(
       function(response) {
-        console.log(response)
+        console.log(response.outputs[0].data.regions.[0].region_info.bounding_box)
       },
       function(err) {
         console.err(err);
@@ -47,7 +49,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* <FaceRecognition /> */}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
