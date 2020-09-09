bf532fabfd77c6ccdb6eccd9d9744d23b73984b2
iahmedsuhail@gmail.com <iahmedsuhail@gmail.com>
2020-09-09 15:34:39 +1200

    Update App.js
    - The app now makes calls to Clarifai API and logs the response in console
    - Todo: Use the response to calculate the where the image is in the picture and display the picture

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Logo from "./components/Logo/Logo.js";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: '20fd979dbbae4bdf9e75eafd84161330'
+});
 
 class App extends React.Component {
   constructor() {
@@ -20,6 +25,16 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     console.log("click");
+
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", ["https://samples.clarifai.com/puppy.jpeg"])
+    .then(
+      function(response) {
+        console.log(response)
+      },
+      function(err) {
+        console.err(err);
+      }
+    );
   };
 
   render() {
